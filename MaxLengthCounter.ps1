22e6532416b767a3cdb7e0e524e4fe2b21abd8a3
Sander Siemonsma <sander.siemonsma@rabobank.com>
2023-05-25 16:44:34 +0200

    MaxLengthCounter

---
+++ MaxLengthCounter.ps1
@@ -0,0 +1,41 @@
+function Get-MaxLengthFromPSObject {
+    <#
+    .SYNOPSIS
+        Get Max lenght from a psobject.
+
+    .DESCRIPTION
+        This will check the length of all cells in a table and output the maximum length. Like the content of a csv file.
+
+    .PARAMETER InputObject
+        PSObject containing the table structure.
+
+    .EXAMPLE
+        $CSVFile=Import-Csv -Path csvfile.csv
+        Get-MaxLengthFromPSObject -InputObject $CSVFile
+
+        This will check all content of the csv file and output the maximum length per column.
+    #>
+
+    param (
+        [Parameter(Mandatory = $true)]
+        [psobject]
+        $InputObject
+    )
+
+    $NotePropertyNames = ($InputObject | Get-Member -MemberType Noteproperty).Name
+    $outputline = "" | Select-Object -Property $NotePropertyNames
+
+    $outputLength = foreach ($InputObjectline in $InputObject) {
+        $outputline = "" | Select-Object -Property ($InputObject | Get-Member -MemberType Noteproperty).Name
+        foreach ($NotePropertyName in $NotePropertyNames) {
+            $outputline.$NotePropertyName = $InputObjectline.$NotePropertyName.Length
+        }
+        $outputline
+    }
+
+    $outputMax = "" | Select-Object -Property $NotePropertyNames
+    foreach ($NotePropertyName in $NotePropertyNames) {
+        $outputMax.$NotePropertyName = ($outputLength.$NotePropertyName | Measure-Object -Maximum).Maximum
+    }
+    $outputMax
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # PSFunctions
 Small PowerShell scripts that can be useful
+
+# MaxLengthCounter.ps1
+This script will grab the content for example from a csv file and output the maximum length per column.
